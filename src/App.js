fe8c2ed1fb342b4557acc6b60ede2980495fff19
aneja5 <153876834+aneja5@users.noreply.github.com>
2024-11-16 14:11:03 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import RulesPage from './components/RulesPage';
 import GamePage from './components/GamePage';
@@ -7,6 +8,7 @@ import GamePage from './components/GamePage';
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/rules" element={<RulesPage />} />
